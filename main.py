45d1efb7c62019a7d0a609c1f9cf46700c1c6b5b
camvaz <camposvazquezv@gmail.com>
2019-11-28 14:04:47 -0600

    Terminado

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from termcolor import colored, cprint
+import sys
 import time
+from termcolor import colored, cprint
 
 class Node():
-    """A node class for A* Pathfinding"""
 
     def __init__(self, parent=None, position=None):
         self.parent = parent
@@ -17,25 +17,19 @@ class Node():
 
 
 def astar(maze, start, end):
-    """Returns a list of tuples as a path from the given start to the given end in the given maze"""
 
-    # Create start and end node
     start_node = Node(None, start)
     start_node.g = start_node.h = start_node.f = 0
     end_node = Node(None, end)
     end_node.g = end_node.h = end_node.f = 0
 
-    # Initialize both open and closed list
     open_list = []
     closed_list = []
 
-    # Add the start node
     open_list.append(start_node)
 
-    # Loop until you find the end
     while len(open_list) > 0:
 
-        # Get the current node
         current_node = open_list[0]
         current_index = 0
         for index, item in enumerate(open_list):
@@ -43,63 +37,49 @@ def astar(maze, start, end):
                 current_node = item
                 current_index = index
 
-        # Pop current off open list, add to closed list
         open_list.pop(current_index)
         closed_list.append(current_node)
 
-        # Found the goal
         if current_node == end_node:
             path = []
             current = current_node
             while current is not None:
                 path.append(current.position)
                 current = current.parent
-            return path[::-1] # Return reversed path
-
-        # Generate children
+            return path[::-1] 
         children = []
-        for new_position in [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)]: # Adjacent squares
+        for new_position in [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)]: 
 
-            # Get node position
             node_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
 
-            # Make sure within range
             if node_position[0] > (len(maze) - 1) or node_position[0] < 0 or node_position[1] > (len(maze[len(maze)-1]) -1) or node_position[1] < 0:
                 continue
 
-            # Make sure walkable terrain
             if maze[node_position[0]][node_position[1]] != 0:
                 continue
 
-            # Create new node
             new_node = Node(current_node, node_position)
 
-            # Append
             children.append(new_node)
 
-        # Loop through children
         for child in children:
 
-            # Child is on the closed list
             for closed_child in closed_list:
                 if child == closed_child:
                     continue
 
-            # Create the f, g, and h values
             child.g = current_node.g + 1
             child.h = ((child.position[0] - end_node.position[0]) ** 2) + ((child.position[1] - end_node.position[1]) ** 2)
             child.f = child.g + child.h
 
-            # Child is already in the open list
             for open_node in open_list:
                 if child == open_node and child.g > open_node.g:
                     continue
 
-            # Add the child to the open list
             open_list.append(child)
 
 
-def main():
+def main(inicio, final):
 
     maze = [[0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
@@ -112,8 +92,11 @@ def main():
             [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
 
-    start = (0, 0)
-    end = (7, 6)
+    coordIni = inicio.split(',');
+    coordFin = final.split(',');
+
+    start = (int(coordIni[0]), int(coordIni[1]));
+    end = (int(coordFin[0]), int(coordFin[1]));    
 
     path = astar(maze, start, end)
 
@@ -126,7 +109,8 @@ def main():
             else:
                 print(f'{j}   ', end='');
         print('\n'); 
-
+    
+    print(f'Buscando de {start} hasta {end}')
     print('Resolviendo');
 
     for i in path:
@@ -147,4 +131,4 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1], sys.argv[2])
---
+++ test.py
@@ -0,0 +1,10 @@
+import sys
+
+a = sys.argv[1];
+
+b = sys.argv[2];
+
+z = a.split(',')
+
+print(z)
+print(b)
